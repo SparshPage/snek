52833e5f453dd5c1b01b117975b11134c6dce862
Sparsh <sparshpage087@gmail.com>
2020-06-01 22:27:08 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,127 @@
+var cvs = document.getElementById("snek");
+var ctx = cvs.getContext("2d");
+
+let snekwidth = 10;
+let snekHeight = 10;
+var initialLen = 4;
+let T = 20;
+const snek = {
+  x: cvs.width / 2 - snekwidth,
+  y: cvs.height / 2,
+  dx: 0,
+  dy: 0,
+};
+
+var sbody = [
+  { x: snek.x, y: snek.y },
+  { x: snek.x + snekwidth, y: snek.y },
+];
+
+function drawSnek() {
+  for (let i = 0; i < sbody.length; i++) {
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    ctx.fillRect(sbody[i].x, sbody[i].y, snekwidth, snekHeight);
+  }
+}
+
+var food = {
+  x: 20,
+  y: 20,
+};
+
+function placefood() {
+  ctx.beginPath();
+  ctx.fillStyle = "red";
+  ctx.fillRect(food.x * snekwidth, food.y * snekHeight, snekwidth, snekHeight);
+}
+
+function keydown(e) {
+  switch (e.keyCode) {
+    case 37:
+      snek.dx = -1;
+      snek.dy = 0;
+
+      break;
+
+    case 38:
+      snek.dy = -1;
+      snek.dx = 0;
+      break;
+    case 39:
+      snek.dx = 1;
+      snek.dy = 0;
+      break;
+    case 40:
+      snek.dy = 1;
+      snek.dx = 0;
+      break;
+  }
+}
+document.addEventListener("keydown", keydown);
+
+function eatFood() {
+  let head = sbody[0];
+
+  if (head.x == food.x * snekwidth && head.y == food.y * snekwidth) {
+    console.log("food eaten");
+    food = {
+      x: Math.floor(Math.random() * 20 + 2),
+      y: Math.floor(Math.random() * 20 + 2),
+    };
+    console.log(food.x * snekwidth, food.y * snekwidth);
+    sbody.unshift({ x: head.x, y: head.y });
+    console.log(sbody.length);
+  }
+}
+
+function die() {
+  for (let i = 1; i < sbody.length; i++) {
+    if (sbody[i].x == sbody[0].x && sbody[i].y == sbody[0].y) {
+      alert("game over bitch");
+      location.reload();
+      break;
+    }
+  }
+}
+
+function bind() {
+  head = sbody[0];
+  if (head.x < 0) {
+    head.x = cvs.width;
+  }
+  if (head.x > cvs.width) {
+    head.x = 0;
+  }
+  if (head.y > cvs.height) {
+    head.y = 0;
+  }
+  if (head.y < 0) {
+    head.y = cvs.height;
+  }
+}
+
+function update() {
+  sbody.pop();
+  sbody.unshift({
+    x: sbody[0].x + snek.dx * snekwidth,
+    y: sbody[0].y + snek.dy * snekHeight,
+  });
+  eatFood();
+  // die();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, cvs.width, cvs.height);
+  drawSnek();
+  placefood();
+}
+
+function loop() {
+  draw();
+  bind();
+  update();
+  setTimeout(requestAnimationFrame(loop), 1000 / 1000);
+}
+
+loop();
